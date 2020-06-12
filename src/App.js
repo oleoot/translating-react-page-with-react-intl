8958e70f5f5ce6fe15f3a554faa01d759399015b
oleoot <oleoot28@gmail.com>
2020-06-12 15:15:17 +0300

    Put Formatter component into translate function, using it to translate on page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import { I18nProvider, LOCALES } from './i18n'
-import { FormattedMessage } from 'react-intl'
+import translate from './i18n/translate'
 
 function App() {
   return (
     <I18nProvider locale={LOCALES.ENGLISH}>
       <div className="App">
         <header className="App-header">
-          <FormattedMessage id="hello" />
+          {translate("hello")}
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
